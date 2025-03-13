ee1164abaa1527df3732635f1c77df4e512ec855
Siwax74 <damien.gey74@gmail.com>
2025-03-13 10:24:07 +0100

    13_03_2025_10:30 - Add Pytest-cov

--- tests/locustfiles.py
+++ tests/locustfiles.py
@@ -3,6 +3,13 @@ from locust import HttpUser, task, between
 class WebsiteUser(HttpUser):
     wait_time = between(1, 3)  # Temps d'attente entre les tâches
 
+    def on_start(self):
+        """Initialisation des données avant chaque tâche"""
+        # Initialiser les variables à l'intérieur de on_start
+        self.email = "john@simplylift.co"
+        self.competition = "Spring Festival"
+        self.club = "Simply Lift"
+
     @task
     def index(self):
         """Accéder à la page d'accueil"""
@@ -11,19 +18,20 @@ class WebsiteUser(HttpUser):
     @task
     def show_summary(self):
         """Simuler une connexion et affichage du récapitulatif"""
-        self.client.post("/showSummary", data={"email": "john@club.com"})
+        self.client.post("/showSummary", data={"email": self.email})
 
     @task
     def book_competition(self):
         """Réserver une compétition"""
-        self.client.get("/book/Spring%20Festival/Simply%20Lift")
+        # Utilisation des variables définies dans on_start
+        self.client.get(f"/book/{self.competition}/{self.club}")
 
     @task
     def purchase_places(self):
         """Acheter des places pour une compétition"""
         self.client.post("/purchasePlaces", data={
-            "competition": "Spring Festival",
-            "club": "Simply Lift",
+            "competition": self.competition,
+            "club": self.club,
             "places": "2"
         })
 
